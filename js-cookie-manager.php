d13c2e32a2f7474b5acff8fdee324756e5b25110
Scott Gustas <scottius@gmail.com>
2016-07-21 12:48:01 -0400

    Update version to 1.1.0

--- js-cookie-manager.php
+++ js-cookie-manager.php
@@ -3,7 +3,7 @@
 
 Plugin Name: 	 JS Cookie Manager
 Description:     This plugin manages cookies via javascript
-Version:         1.0
+Version:         1.1.0
 Author:          Terri Regan
 License:         GPL-2.0+
 Text Domain:     america
@@ -17,17 +17,17 @@ defined( 'ABSPATH' ) || exit;
 
 class JS_Cookie_Manager {
 
-	const PLUGIN_VERSION = '1.0.0';
+	const PLUGIN_VERSION = '1.1.0';
 
 	function bootstrap() {
 		add_action( 'wp_enqueue_scripts',  array( $this, 'iip_enqueue_scripts' ) );
 	}
 
 	function iip_enqueue_scripts() {
-		wp_register_script('js-cookie-manager', plugin_dir_url( __FILE__ ) . 'js/dist/js-cookie-manager.min.js', array(),  '1.0.0', true );
+		wp_register_script('js-cookie-manager', plugin_dir_url( __FILE__ ) . 'js/dist/js-cookie-manager.min.js', array(),  '1.1.0', true );
 
 		wp_enqueue_script( 'js-cookie-manager');
-		wp_enqueue_script( 'lang-cookie-listener', plugin_dir_url( __FILE__ ) . 'js/dist/lang-cookie-listener.min.js', array('jquery', 'js-cookie-manager'), '1.0.0', true );
+		wp_enqueue_script( 'lang-cookie-listener', plugin_dir_url( __FILE__ ) . 'js/dist/lang-cookie-listener.min.js', array('jquery', 'js-cookie-manager'), '1.1.0', true );
 	}
 }
 
